f67649f7d03b49a8533ac16fb61ea301702ce275
Ramez AL_saadi <ramzalsady96@gmail.com>
2026-07-16 19:40:34 +0300

    Update main.cpp

--- Password-Generator/main.cpp
+++ Password-Generator/main.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+class PasswordGenerator
+{
+private:
+    string username;
+    string symbols = "!@#$%^&*";
+    string numbers = "0123456789";
+
+public:
+
+    void setUsername()
+    {
+        cout << "Enter username: ";
+        cin >> username;
+    }
+
+    string generatePassword()
+    {
+        string password = username;
+
+        // نضيف أرقام ورموز بشكل عشوائي
+        for (int i = 0; i < 4; i++)
+        {
+            int numIndex = rand() % numbers.length();
+            int symIndex = rand() % symbols.length();
+
+            password += numbers[numIndex];
+            password += symbols[symIndex];
+        }
+
+        return password;
+    }
+
+    void showPassword()
+    {
+        cout << "Generated Strong Password: "
+             << generatePassword() << endl;
+    }
+};
+
+int main()
+{
+    srand(time(0));
+
+    PasswordGenerator p;
+
+    p.setUsername();
+    p.showPassword();
+
+    return 0;
+}
